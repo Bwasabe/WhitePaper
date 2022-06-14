3d4844d4d71d634349bbd6a748d501658dcf75a2
Bwasabe <jixg20@gmail.com>
2022-06-14 09:24:28 +0900

    06/14 UI Package 추가

---
+++ Assets/01.Scripts/03.Bases/Item/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string _itemName;
+
+
+    //TODO: 스테이터스가 들어가겠지
+
+    [SerializeField]
+    private GameObject _imageObject;
+}
